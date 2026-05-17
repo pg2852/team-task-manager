b256ad27ce99b7f8fed4b3f2909a863792f1cfaf
Parth <paarthgupta2003@gmail.com>
2026-05-17 22:50:49 +0530

    Fixed task model import

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const Task = require("../models/Task")
+const Task = require("../models/task")
 
 router.post("/create", async(req,res)=>{
 
